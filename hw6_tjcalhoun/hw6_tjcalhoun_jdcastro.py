a34fbec8218a164ccc0240c0d1f6a7e01cd8dca0
DarkLeodin <jocastro42@gmail.com>
2016-02-22 09:46:30 -0900

    Added files via upload

--- hw6_tjcalhoun/hw6_tjcalhoun_jdcastro.py
+++ hw6_tjcalhoun/hw6_tjcalhoun_jdcastro.py
@@ -1,4 +1,44 @@
-# Thomas Calhoun & Jose Castro
-# homework #6
-# tjcalhoun@alaska.edu & jdcastro@alaska.edu 18 Feb 2016 to 25 Feb 2016
-# Python 3.5.1
+import random
+
+from random import randint
+
+random = randint(1, 20)
+
+# The Welcome Screen
+
+print("Welcome to the number guessing game!")
+print()
+Userinput = input("Please tell me your name:")
+Useroutput = str(Userinput)
+print()
+
+print("\n")
+print("I am thinking of a number between 1 and 20" )
+print()
+print("Can you guess it?")
+
+print("\n")
+
+num = int(input("Enter a number between [1-20]:"))
+
+while num != random:
+
+    if num > random:
+
+        print ("Too High")
+        num = int(input("Enter a number [1-20]: "))
+        continue
+
+    elif num < random:
+        print ("Too Low")
+        num = int(input("Enter a number [1-20]: "))
+        continue
+
+# The Correct Guess
+
+print("\n")
+print("Congratualtions YOU WIN!!!")
+print()
+print("Thank you " + Useroutput + " for your participation!")
+print("\n")
+input("Press RETURN to exit..")
